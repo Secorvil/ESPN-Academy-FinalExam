a3496caecd020e90932f8afa6e95d89483698f9d
Sebastian Correa Villada <sebastian.cv@globant.com>
2022-05-18 17:02:54 -0500

    Login and Sign-up tests finished

--- src/test/java/com/globant/screens/HomePage.java
+++ src/test/java/com/globant/screens/HomePage.java
@@ -18,6 +18,33 @@ public class HomePage extends BasePage {
     @FindBy(id = "logo")
     private WebElement LogInESPNLogo;
 
+    @FindBy(css = "button[data-testid=\"BtnSubmit\"]")
+    private WebElement frameLogInButton;
+
+    @FindBy(css = "button[data-testid=\"BtnCreateAccount\"]")
+    private WebElement frameSignUpButton;
+
+    @FindBy(css = "h2[data-testid=\"Title\"]")
+    private WebElement frameSignUpTitle;
+
+    @FindBy(className = "input-InputFirstName")
+    private WebElement firstNameInput;
+
+    @FindBy(className = "input-InputLastName")
+    private WebElement lastNameInput;
+
+    @FindBy(className = "input-InputEmail")
+    private WebElement emailInput;
+
+    @FindBy(className = "input-password-new")
+    private WebElement passwordInput;
+
+    @FindBy(css = "button[data-testid=\"BtnSubmit\"]")
+    private WebElement submitSignUpButton;
+
+    @FindBy(css = "button[data-testid=\"REGISTRATION-close\"]")
+    private WebElement closeButton;
+
     /**
      * Basic constructor.
      *
@@ -57,6 +84,150 @@ public class HomePage extends BasePage {
      */
     public void setLogInESPNLogo(WebElement logInESPNLogo) {
         LogInESPNLogo = logInESPNLogo;
+    }
+
+    /**
+     * Get frame "Log In" button.
+     * @return WebElement of the frame "Log In" button
+     */
+    public WebElement getFrameLogInButton() {
+        return frameLogInButton;
+    }
+
+    /**
+     * Set a WebElement as frame "Log In" button.
+     * @param frameLogInButton new frameLogInButton element
+     */
+    public void setFrameLogInButton(WebElement frameLogInButton) {
+        this.frameLogInButton = frameLogInButton;
+    }
+
+    /**
+     * Get frame "Sign Up" button.
+     * @return WebElement of the frame "Sign up" button
+     */
+    public WebElement getFrameSignUpButton() {
+        return frameSignUpButton;
+    }
+
+    /**
+     * Set a WebElement as frame "Sign up" button.
+     * @param frameSignUpButton new frameSignUpButton element
+     */
+    public void setFrameSignUpButton(WebElement frameSignUpButton) {
+        this.frameSignUpButton = frameSignUpButton;
+    }
+
+    /**
+     * Get frame title.
+     * @return WebElement of frame title
+     */
+    public WebElement getFrameSignUpTitle() {
+        return frameSignUpTitle;
+    }
+
+    /**
+     * Set a WebElement as frame title.
+     * @param frameSignUpTitle new frameSignUpTitle element
+     */
+    public void setFrameSignUpTitle(WebElement frameSignUpTitle) {
+        this.frameSignUpTitle = frameSignUpTitle;
+    }
+
+    /**
+     * Get the sign-up form "First name" field.
+     * @return WebElement of the "First name" input
+     */
+    public WebElement getFirstNameInput() {
+        return firstNameInput;
+    }
+
+    /**
+     * Set a WebElement as "First name" input.
+     * @param firstNameInput new firstNameInput element
+     */
+    public void setFirstNameInput(WebElement firstNameInput) {
+        this.firstNameInput = firstNameInput;
+    }
+
+    /**
+     * Get the sign-up form "Last name" field.
+     * @return WebElement of the "Last name" input
+     */
+    public WebElement getLastNameInput() {
+        return lastNameInput;
+    }
+
+    /**
+     * Set a WebElement as "Last name" input.
+     * @param lastNameInput new lastNameInput element
+     */
+    public void setLastNameInput(WebElement lastNameInput) {
+        this.lastNameInput = lastNameInput;
+    }
+
+    /**
+     * Get the sign-up form "Email" field.
+     * @return WebElement of the "Email" input
+     */
+    public WebElement getEmailInput() {
+        return emailInput;
+    }
+
+    /**
+     * Set a WebElement as "Email" input.
+     * @param emailInput new emailInput element
+     */
+    public void setEmailInput(WebElement emailInput) {
+        this.emailInput = emailInput;
+    }
+
+    /**
+     * Get the sign-up form "Password" field.
+     * @return WebElement of the "Password" input
+     */
+    public WebElement getPasswordInput() {
+        return passwordInput;
+    }
+
+    /**
+     * Set a WebElement as "Password" input.
+     * @param passwordInput new passwordInput
+     */
+    public void setPasswordInput(WebElement passwordInput) {
+        this.passwordInput = passwordInput;
+    }
+
+    /**
+     * Get the sign-up form submit button.
+     * @return WebElement of "Sign Up" submit button
+     */
+    public WebElement getSubmitSignUpButton() {
+        return submitSignUpButton;
+    }
+
+    /**
+     * Set a WebElement as "Sign Up" submit button.
+     * @param submitSignUpButton new submitSignUpButton element
+     */
+    public void setSubmitSignUpButton(WebElement submitSignUpButton) {
+        this.submitSignUpButton = submitSignUpButton;
+    }
+
+    /**
+     * Get the close (X) frame button.
+     * @return WebElement of close frame button
+     */
+    public WebElement getCloseButton() {
+        return closeButton;
+    }
+
+    /**
+     * Set a WebElement as close frame button.
+     * @param closeButton new closeButton element
+     */
+    public void setCloseButton(WebElement closeButton) {
+        this.closeButton = closeButton;
     }
 
     /**
--- src/test/java/com/globant/tests/ESPNTest.java
+++ src/test/java/com/globant/tests/ESPNTest.java
@@ -3,13 +3,40 @@ package com.globant.tests;
 import com.globant.util.tests.BaseTest;
 import org.testng.Assert;
 import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
 
 public class ESPNTest extends BaseTest {
 
     @Test
     public void loginElementsExistTest() {
+        log.info("Clicking \"Log In\" button");
         home.clickSideLogInButton();
+        log.info("Switching to Login frame");
         driver.getDriver().switchTo().frame("oneid-iframe");
+        log.info("Assert that logo and buttons are present");
         Assert.assertTrue(home.isElementVisible(home.getLogInESPNLogo()));
+        Assert.assertTrue(home.isElementVisible(home.getFrameLogInButton()));
+        Assert.assertTrue(home.isElementVisible(home.getFrameSignUpButton()));
+    }
+
+    @Test
+    public void signupElementsExistTest() {
+        log.info("Clicking \"Log In\" button");
+        home.clickSideLogInButton();
+        log.info("Switching to Login frame");
+        driver.getDriver().switchTo().frame("oneid-iframe");
+        log.info("Clicking frame \"Sign Up\" button");
+        home.click(home.getFrameSignUpButton());
+        log.info("Assert that title of \"Create Account\" frame matches expected title");
+        Assert.assertEquals(home.getFrameSignUpTitle().getText(), "Create Account", "Expected title and actual title don't match");
+        log.info("Assert each element of Sign Up form is present and visible");
+        SoftAssert softAssert = new SoftAssert();
+        softAssert.assertTrue(home.isElementVisible(home.getFirstNameInput()), "First name input is not present nor visible");
+        softAssert.assertTrue(home.isElementVisible(home.getLastNameInput()), "Last name input is not present nor visible");
+        softAssert.assertTrue(home.isElementVisible(home.getEmailInput()), "Email input is not present nor visible");
+        softAssert.assertTrue(home.isElementVisible(home.getPasswordInput()), "Password input is not present nor visible");
+        softAssert.assertTrue(home.isElementVisible(home.getSubmitSignUpButton()), "Sign up button is not present nor visible");
+        softAssert.assertTrue(home.isElementVisible(home.getCloseButton()), "Close button is not present nor visible");
+        softAssert.assertAll();
     }
 }
